9c3ce08f9a05eac2f2da9c40dc00ff804949fd33
Sebastian Konicz <sebastian.konicz@gmail.com>
2021-01-16 15:35:58 +0100

    Scraped the table of municipalietes form Wiki page

--- src/PNA_01_scrapper_wiki.py
+++ src/PNA_01_scrapper_wiki.py
@@ -1,9 +1,10 @@
 from pathlib import Path
+from bs4 import BeautifulSoup
+import os
+import re
+import requests
 import pandas as pd
 import time
-import tabula
-import pdfplumber
-from IPython.display import Image, display
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
@@ -15,35 +16,68 @@ def main():
     # project directory
     project_dir = str(Path(__file__).resolve().parents[1])
 
-    # file = r'C:\Users\sebas\OneDrive\Pulpit\ZIP\PNA.pdf'
-    # official link to the document
-    link = r'https://www.poczta-polska.pl/hermes/uploads/2013/11/spispna.pdf'
+    # gettig the main page with municipality divide table
+    main_page = requests.get('https://pl.wikipedia.org/wiki/Lista_gmin_w_Polsce')
+    html_main_page = BeautifulSoup(main_page.content, 'html.parser')
 
-    # empty table list
-    tables_list = []
+    table_body = html_main_page.find("table", class_='sortable wikitable')
+    table_rows = table_body.find_all("tr")
+
+    # empty lists for values
+    teryt_list = []
+    mun_list = []
+    pow_list = []
+    woj_list = []
 
     # looping throug tabels on page from page 3 to
-    for i in range(4, 1649):
-        table = tabula.read_pdf(link, pages=i)
-        table_df = table[0]
-        print("table form page {}".format(i))
-        # renaming columna
-        table_df.columns = ["PNA", "ADRES", "WOJ"]
-        # dropping first row
-        table_df = table_df.iloc[1:]
-        tables_list.append(table_df)
+    for row in table_rows:
+        # headers
+        col_head = row.find_all('th')
+        # values
+        col_boady = row.find_all('td')
 
-    # concatenating dataframes
-    zipcodes = pd.concat(tables_list, axis=0, sort=False)
+        if len(col_head) != 0:
+            pass
+        else:
+            # getting values for specific columns
+            teryt = col_boady[0].text.rstrip('\n')
+            mun = col_boady[1].text.rstrip('\n')
+            pow = col_boady[2].text.rstrip('\n')
+            woj = col_boady[3].text.rstrip('\n')
+            # appending values to the list
+            teryt_list.append(teryt)
+            mun_list.append(mun)
+            pow_list.append(pow)
+            woj_list.append(woj)
+
+    # zipping lists
+    data_tuples = list(zip(teryt_list, mun_list, pow_list, woj_list))
+
+    # creating dataframe
+    municipalities = pd.DataFrame(data_tuples, columns=["teryt", "municipality", "county", "voivodeship"])
+
+    # removing name "Gmina" from col municipality
+    municipalities['municipality'] = municipalities['municipality'].map(lambda mun: mun_remv(mun))
+
+    print(municipalities)
 
     # saving dataframe
     print("saving files")
-    zipcodes.to_csv(project_dir + r'\data\interim\01_zipcodes.csv', index=False, encoding='UTF-8')
+    municipalities.to_csv(project_dir + r'\data\interim\01_municipalities.csv', index=False, encoding='UTF-8')
 
     # end time of program + duration
     end_time = time.time()
     execution_time = int(end_time - start_time)
     print('\n', 'exectution time = ', execution_time, 'sec')
+
+
+def mun_remv(mun):
+    word = "gmina "
+    if mun.find(word) != -1:
+        mun = mun.replace(word, "")
+    else:
+        pass
+    return mun
 
 if __name__ == "__main__":
     main()
