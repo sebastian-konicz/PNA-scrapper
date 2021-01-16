a31af9931ebe151190ba301b84a5d993e3a7b4ef
Sebastian Konicz <sebastian.konicz@gmail.com>
2021-01-16 11:26:46 +0100

    Divided scripts to two typical pdf scrappers. Will use tabula for now as ith has better view of the table - dataframe

--- src/PNA_01_scrapper.py
+++ src/PNA_01_scrapper.py
@@ -3,6 +3,7 @@ import pandas as pd
 import time
 import tabula
 import pdfplumber
+from IPython.display import Image, display
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
@@ -14,51 +15,53 @@ def main():
     # project directory
     project_dir = str(Path(__file__).resolve().parents[1])
 
-    file = r'C:\Users\sebas\OneDrive\Pulpit\ZIP\PNA.pdf'
+    # file = r'C:\Users\sebas\OneDrive\Pulpit\ZIP\PNA.pdf'
     # official link to the document
-    # link = r'https://www.poczta-polska.pl/hermes/uploads/2013/11/spispna.pdf'
+    link = r'https://www.poczta-polska.pl/hermes/uploads/2013/11/spispna.pdf'
 
 
-    with pdfplumber.open(file) as pdf:
-        first_page = pdf.pages[3]
-        print(first_page.to_image())
-        # print(first_page.images)
+    # with pdfplumber.open(file) as pdf:
+    #     first_page = pdf.pages[3]
+    #     table = first_page.extract_table()
+    #     print(table)
+    #     im = first_page.to_image()
+    #     im1 = im.draw_rects(first_page.extract_words())
+    #     im2 = im.debug_tablefinder()
+    #     im2.save(r'C:\Users\sebas\OneDrive\Pulpit\PNA-scrapper\data\interim\image.jpg')
+    #     display(Image(im))
 
     # pages with zip code table 3 -
-    # table_start = tabula.read_pdf(link, pages=3)
-    # table_middle_1 = tabula.read_pdf(link,
-    #                                  pages=4,
-    #                                  area=[0, 0, 100, 100])
-    # table_middle_2 = tabula.read_pdf(link, pages=100)
-    # table_middle_3 = tabula.read_pdf(link, pages=824)
-    # table_end = tabula.read_pdf(link, pages=1648)
+    table_start = tabula.read_pdf(link, pages=3)
+    table_middle_1 = tabula.read_pdf(link, pages=4)
+    table_middle_2 = tabula.read_pdf(link, pages=100)
+    table_middle_3 = tabula.read_pdf(link, pages=824)
+    table_end = tabula.read_pdf(link, pages=1648)
 
-    # print("first table")
-    # print(table_start[0])
-    # print("middle table 1")
-    # print(table_middle_1[0])
-    # print("middle table 2")
-    # print(table_middle_2[0])
-    # print("middle table 3")
-    # print(table_middle_3[0])
-    # print("last table")
-    # print(table_end[0])
+    print("first table")
+    print(table_start[0].head())
+    print("middle table 1")
+    print(table_middle_1[0].head())
+    print("middle table 2")
+    print(table_middle_2[0].head())
+    print("middle table 3")
+    print(table_middle_3[0].head())
+    print("last table")
+    print(table_end[0].head())
 
 
-    # table_start = table_start[0]
-    # table_middle_1 = table_middle_1[0]
-    # table_middle_2 = table_middle_2[0]
-    # table_middle_3 = table_middle_3[0]
-    # table_end = table_end[0]
+    table_start = table_start[0]
+    table_middle_1 = table_middle_1[0]
+    table_middle_2 = table_middle_2[0]
+    table_middle_3 = table_middle_3[0]
+    table_end = table_end[0]
 
     # saving dataframe
     print("saving files")
-
-    # table_start.to_csv(project_dir + r'\data\interim\0_table_start.csv', index=False, encoding='UTF-8')
-    # table_middle_1.to_csv(project_dir + r'\data\interim\1_table_middle_1.csv', index=False, encoding='UTF-8')
-    # table_middle_2.to_csv(project_dir + r'\data\interim\1_table_middle_2.csv', index=False, encoding='UTF-8')
-    # table_middle_3.to_csv(project_dir + r'\data\interim\1_table_middle_3.csv', index=False, encoding='UTF-8')
-    # table_end.to_csv(project_dir + r'\data\interim\2_table_end.csv', index=False, encoding='UTF-8')
+    table_start.to_csv(project_dir + r'\data\interim\0_table_start.csv', index=False, encoding='UTF-8')
+    table_middle_1.to_csv(project_dir + r'\data\interim\1_table_middle_1.csv', index=False, encoding='UTF-8')
+    table_middle_2.to_csv(project_dir + r'\data\interim\1_table_middle_2.csv', index=False, encoding='UTF-8')
+    table_middle_3.to_csv(project_dir + r'\data\interim\1_table_middle_3.csv', index=False, encoding='UTF-8')
+    table_end.to_csv(project_dir + r'\data\interim\2_table_end.csv', index=False, encoding='UTF-8')
 
     # end time of program + duration
     end_time = time.time()
