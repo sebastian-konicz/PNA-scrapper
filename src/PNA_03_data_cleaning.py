d2385ed42475c91012aa1d6dd4d6f626624f746c
Sebastian Konicz <sebastian.konicz@gmail.com>
2021-01-16 20:58:12 +0100

    Have new idea how to precisly match the county and municipality - just concatenate the values and search through adress colum

--- src/PNA_03_data_cleaning.py
+++ src/PNA_03_data_cleaning.py
@@ -16,7 +16,7 @@ def main():
     project_dir = str(Path(__file__).resolve().parents[1])
 
     # dataframe with 'dirty' data
-    zip_path = r'\data\interim\01_zipcodes_copy.csv'
+    zip_path = r'\data\interim\01_zipcodes.csv'
 
     # dataframe with 'municipalities data
     mun_path = r'\data\interim\02_municipalities.csv'
@@ -24,7 +24,6 @@ def main():
     # read dataframes
     zip = pd.read_csv(project_dir + zip_path)
     mun = pd.read_csv(project_dir + mun_path)
-
 
     # MUNICIPALITIES DATA
     # list of municipalities (in lovercase)
@@ -62,19 +61,20 @@ def main():
     # ADRESS - to lowercase
     zip['ADRESS'] = zip['ADRESS'].apply(lambda text: text.lower())
 
-    # COU - extracting right municipality
-    zip['COU'] = ""
-    # comapring counties with the counties in ADRESS
-    zip["COU"] = zip.apply(lambda zip: county_match(zip["ADRESS"], counties), axis=1)
+    # # COU - extracting right municipality
+    # zip['COU'] = ""
+    # # comapring counties with the counties in ADRESS
+    # zip["COU"] = zip.apply(lambda zip: county_match(zip["ADRESS"], counties), axis=1)
 
     # ADRESS_2 - cleaned column for better municipality association
     zip['ADRESS_2'] = ""
-    zip['ADRESS_2'] = zip.apply(lambda zip: zip["ADRESS"].replace(zip["COU"], ''), axis=1)
+    # strippirng counties from adress column
+    zip['ADRESS_2'] = zip.apply(lambda zip: strippping_counties(zip["ADRESS"], zip["COU"]), axis=1)
 
-    # MUN - extracting right municipality
-    zip['MUN'] = ""
-    # comapring municipalites with the municipalities in ADRESS
-    zip["MUN"] = zip.apply(lambda zip: municip_match(zip["ADRESS_2"], municipalities), axis=1)
+    # # MUN - extracting right municipality
+    # zip['MUN'] = ""
+    # # comapring municipalites with the municipalities in ADRESS
+    # zip["MUN"] = zip.apply(lambda zip: municip_match(zip["ADRESS_2"], municipalities), axis=1)
 
     # saving dataframe
     print("saving file")
@@ -88,43 +88,60 @@ def main():
 # extracting zip-code do PNA
 def zip_code_extr(statement):
     pattern_zip = re.compile("([0-9])\w+-([0-9])\w+")
-    zip_code = re.search(pattern_zip, statement).group()
+    zip_code = ''
+    if type(pattern_zip.search(statement)) == re.Match:
+        zip_code = re.search(pattern_zip, statement).group()
+    else:
+        pass
     return zip_code
 
 # removing zip-code form CITY
 def zip_code_remv(statement):
     pattern_zip = re.compile("([0-9])\w+-([0-9])\w+")
+    city = ''
     if type(pattern_zip.search(statement)) == re.Match:
         city = statement.replace(pattern_zip.search(statement).group(), "")
+    else:
+        pass
     return city
 
-# finding match for municipality in ADRESS_2 column
-def municip_match(text, municipalities):
-    result = ''
-    for mun in municipalities:
-        ratio = fuzz.partial_ratio(text, mun)
-        if ratio == 100:
-            print(text)
-            print(mun)
-            print(ratio)
-            result = mun
-        else:
-            pass
-    return result
+# # finding match for municipality in ADRESS_2 column
+# def municip_match(text, municipalities):
+#     result = ''
+#     for mun in municipalities:
+#         ratio = fuzz.partial_ratio(text, mun)
+#         if (ratio == 100) & (len(mun) <= len(text)):
+#             # print(len(mun))
+#             # print(len(text))
+#             # print(text)
+#             # print(mun)
+#             # print(ratio)
+#             result = mun
+#         else:
+#             pass
+#     return result
 
-# finding match for county in ADRESS column
-def county_match(text, counties):
-    result = ''
-    for cou in counties:
-        ratio = fuzz.partial_ratio(text, cou)
-        if ratio == 100:
-            print(text)
-            print(cou)
-            print(ratio)
-            result = cou
-        else:
-            pass
-    return result
+# # finding match for county in ADRESS column
+# def county_match(text, counties):
+#     result = ''
+#     for cou in counties:
+#         ratio = fuzz.partial_ratio(text, cou)
+#         if (ratio == 100):
+#             # print(text)
+#             # print(cou)
+#             # print(ratio)
+#             result = cou
+#         else:
+#             pass
+#     return result
+
+def strippping_counties(text, county):
+    if text.find(county) != -1:
+        index = text.find(county)
+        text = text[: index]
+    else:
+        pass
+    return text
 
 if __name__ == "__main__":
     main()
